0eeb6ace712ecb859bea1dba67dff6871668679a
Javier Martín Alonso <xavimartin@hotmail.com>
2016-10-03 09:50:19 +0200

    COMENTARIOS

--- example_spring_boot/src/main/java/es/example/boot/spring/controller/Example.java
+++ example_spring_boot/src/main/java/es/example/boot/spring/controller/Example.java
@@ -4,15 +4,19 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Indica a spring que es un controlador rest
 @RestController
+//Indicamos a springboot que esta es la clase por la que debe arrancar
 @EnableAutoConfiguration
 public class Example {
 
-    @RequestMapping("/")
+    //Como controlador rest que es. Este metodo debe responder a esta url
+	@RequestMapping("/")
     String home() {
         return "Hello World!";
     }
 
+	//Metodo necesario para arrancar con Spring boot
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
     }
