530e7b79a571ce44fe8a8e5b342753ad704d5c20
Javier Martín Alonso <xavimartin@hotmail.com>
2017-10-17 13:21:05 +0200

    creación del modulo con el wizard de spring sts

---
+++ spring-boot-ws/src/main/java/es/jmartin/java/SpringBootWsApplication.java
@@ -0,0 +1,12 @@
+package es.jmartin.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWsApplication.class, args);
+	}
+}
