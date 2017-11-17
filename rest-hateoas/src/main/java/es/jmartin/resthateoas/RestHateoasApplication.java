1f9762c43b4677e1a379aef1cfb97b509e6765cc
Javier Martín Alonso <xavimartin@hotmail.com>
2017-11-17 18:16:44 +0100

    creacion del esqueleto del módulo con spring-started-project

---
+++ rest-hateoas/src/main/java/es/jmartin/resthateoas/RestHateoasApplication.java
@@ -0,0 +1,12 @@
+package es.jmartin.resthateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHateoasApplication.class, args);
+	}
+}
